5c973a45de685b95bd138c7803f14019d0e39b36
Daniel Pavel <daniel.pavel19@gmail.com>
2024-10-30 13:29:43 +0200

    add native-vault instructions handler

---
+++ native-vault/src/instructions.rs
@@ -0,0 +1,18 @@
+use solana_program::program_error::ProgramError;
+
+pub enum VaultInstructions {
+    Deposit,
+    Withdraw,
+}
+
+impl TryFrom<&u8> for VaultInstructions {
+    type Error = ProgramError;
+
+    fn try_from(value: &u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(VaultInstructions::Deposit),
+            1 => Ok(VaultInstructions::Withdraw),
+            _ => Err(ProgramError::InvalidInstructionData),
+        }
+    }
+}
--- native-vault/src/lib.rs
+++ native-vault/src/lib.rs
@@ -1,14 +1,35 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use instructions::VaultInstructions;
+use solana_program::{
+    account_info::AccountInfo,
+    entrypoint::{entrypoint, ProgramResult},
+    program_error::ProgramError,
+    pubkey::{pubkey, Pubkey},
+};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+mod instructions;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+const ID: Pubkey = pubkey!("22222222222222222222222222222222");
+
+entrypoint!(process_instruction);
+
+fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    data: &[u8],
+) -> ProgramResult {
+    if program_id.ne(&crate::ID) {
+        return Err(ProgramError::IncorrectProgramId);
     }
+
+    // Figure out which instruction we're calling
+    let (discriminator, data) = data
+        .split_first()
+        .ok_or(ProgramError::InvalidInstructionData)?;
+
+    match VaultInstructions::try_from(discriminator)? {
+        VaultInstructions::Deposit => todo!(),
+        VaultInstructions::Withdraw => todo!(),
+    };
+
+    Ok(())
 }
